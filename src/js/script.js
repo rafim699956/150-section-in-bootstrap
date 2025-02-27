d4525ea8e5e6e608d7a24eeb6c92aa17dc10179a
unknown <rafim699956@gmail.com>
2025-02-27 21:45:30 +0600

    create zigzag banner section with bootstarp and custom css

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+var splide = new Splide('.splide', {
+    perPage: 3,
+    rewind: true,
+    arrows: false,
+    gap: '1.875rem',
+    focus: 'center',
+    padding: '0',
+    speed: 500,
+    type: 'loop',
+   });
+
+splide.mount();
